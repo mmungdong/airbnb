8e0249ab963f5f74a7bc10afced702dc3308804c
Mungdong <yangfb510195171@gmail.com>
2024-11-04 23:45:49 +0800

    初始化项目 1. 创建项目目录 2. 增加一些craco和less配置项

---
+++ craco.config.js
@@ -0,0 +1,22 @@
+// 别名和 less 配置
+const path = require('path');
+const CracoLessPlugin = require('craco-less');
+
+
+const resolve = pathname => path.resolve(__dirname, pathname);
+
+module.exports = {
+    // less
+    plugins: [
+        {
+            plugin: CracoLessPlugin
+        }
+    ],
+    // webpack: 这里的配置会覆盖掉 webpack 中的别名配置
+    webpack: {
+        alias: {
+            '@': resolve('src'),
+            'components': resolve('src/components'),
+        }  
+    }
+}
