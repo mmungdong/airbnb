5b782785f14dd54a9ed66eb0ce771bd41962c647
Mungdong <yangfb510195171@gmail.com>
2024-11-05 22:30:24 +0800

    redux 状态管理配置

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import App from './App';
 import './assets/css/index.less'
+import store from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/*  Suspense 用于解决路由懒加载，因为该加载为异步 https://zh-hans.react.dev/reference/react/Suspense */}
     <Suspense fallback={<div>loading...</div>} >
-      <HashRouter>
-        <App />
-      </HashRouter>
+      {/*  Provider 用于提供 store 给组件 */}
+      <Provider store={store}>
+        <HashRouter>
+          <App />
+        </HashRouter>
+      </Provider>
     </Suspense>
   </React.StrictMode>
 );
---
+++ src/store/modules/entire/reducer.js
@@ -0,0 +1,15 @@
+import { current } from "@reduxjs/toolkit"
+
+const initialState = {
+  
+}
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+
+    default:
+      return state
+  }
+}
+
+export default reducer
