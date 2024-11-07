dbacc64585ad4ecc2f61369a62d8e1fd7b8734ff
Mungdong <yangfb510195171@gmail.com>
2024-11-07 21:58:40 +0800

    react css 编写实践

---
+++ src/components/app-header/style.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'  // install: npm install styled-components
+
+export const AppHeaderWrapper = styled.div`
+  .left {
+    color: blue;
+  }
+
+  .center {
+    color: orange;
+  }
+
+  .right {
+    color: green;
+  }
+`
